4cb461dd74b84088182d9f6fe1cc477862d28619
Dev Tanna <dt@int.consol.ae>
2019-06-11 00:22:27 +0400

    removing excess logging from scripts.

--- docker/scraper/run_all_scrapers_script.sh
+++ docker/scraper/run_all_scrapers_script.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 export ENV="docker" && export NODE_PATH="/app/node_modules"
 echo "---starting script---" && date && date >> /var/log/cron.log && echo "---starting script---" >> /var/log/cron.log &&
-echo "< locations..." && /usr/local/bin/node /app/scraper/locations.js >> /var/log/cron.log && echo "... locations >"
+echo "< locations..." && /usr/local/bin/node /app/scraper/locations.js && echo "... locations >"
 wait
 
-echo "< talabat ..." && /usr/local/bin/node /app/scraper/talabat.js >> /var/log/cron.log && echo "... talabat >" && /usr/local/bin/node /app/scraper/reindex_offers.js >> /var/log/cron.log &
-echo "< ubereats ..." && /usr/local/bin/node /app/scraper/ubereats.js >> /var/log/cron.log && echo "... ubereats >" && /usr/local/bin/node /app/scraper/reindex_offers.js >> /var/log/cron.log &
-echo "< deliveroo ..." && /usr/local/bin/node /app/scraper/deliveroo.js >> /var/log/cron.log && echo "... deliveroo >" && /usr/local/bin/node /app/scraper/reindex_offers.js >> /var/log/cron.log &
-echo "< carriage ..." && /usr/local/bin/node /app/scraper/carriage.js >> /var/log/cron.log && echo "... carriage >" && /usr/local/bin/node /app/scraper/reindex_offers.js >> /var/log/cron.log &
-echo "< zomato ..." && /usr/local/bin/node /app/scraper/zomato.js >> /var/log/cron.log && echo "... zomato >" && /usr/local/bin/node /app/scraper/reindex_offers.js >> /var/log/cron.log &
+echo "< talabat ..." && /usr/local/bin/node /app/scraper/talabat.js && echo "... talabat >" && /usr/local/bin/node /app/scraper/reindex_offers.js &
+echo "< ubereats ..." && /usr/local/bin/node /app/scraper/ubereats.js && echo "... ubereats >" && /usr/local/bin/node /app/scraper/reindex_offers.js &
+echo "< deliveroo ..." && /usr/local/bin/node /app/scraper/deliveroo.js && echo "... deliveroo >" && /usr/local/bin/node /app/scraper/reindex_offers.js &
+echo "< carriage ..." && /usr/local/bin/node /app/scraper/carriage.js && echo "... carriage >" && /usr/local/bin/node /app/scraper/reindex_offers.js &
+echo "< zomato ..." && /usr/local/bin/node /app/scraper/zomato.js && echo "... zomato >" && /usr/local/bin/node /app/scraper/reindex_offers.js &
 
 wait
 date && date >> /var/log/cron.log
