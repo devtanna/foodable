b03b9f144c5393e4ebd3bd5cc1858f688452256e
Dev Tanna <dt@int.consol.ae>
2019-06-24 16:27:17 +0400

    Correcting .env variables for next [image-build]

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
 const withCSS = require('@zeit/next-css');
 
 module.exports = Object.assign(
   withCSS({
     cssModules: false,
     webpack(config, options) {
+      config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
       config.module.rules.push({
         test: /\.(png|woff|woff2|eot|ttf|svg|gif|jpg)$/,
         loader: 'file-loader',
