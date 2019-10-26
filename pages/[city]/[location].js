17886d9346c9870e4fe296c9649d5c6c887d2636
arayess <therayess@gmail.com>
2019-10-26 13:37:41 +0400

    [azure-deploy]: revert

--- pages/[city]/[location].js
+++ pages/[city]/[location].js
@@ -70,6 +70,7 @@ const Location = ({
 
   return (
     <Fragment>
+      <PageHead page={page} location={selectedLocation} filters={searchFilters} />
       <div className="wrapper">
         {(device === 'phone' || device === 'tablet') && (
           <FoodablesMobile
