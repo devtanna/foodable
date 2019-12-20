d5712c318bee143cfb54eb2c79c0388686a2a1bf
dev <dev@dubizzle.com>
2019-12-20 13:22:12 +0400

    editing scrapeall script

--- scrapeAll.sh
+++ scrapeAll.sh
@@ -22,52 +22,53 @@ wait
 export NODE_PATH="$PWD/node_modules"
 start=`date +%s`
 
-node scraper/cleanup/dbClean.js
+echo "< dbclean ..." && node scraper/cleanup/dbClean.js && echo "... dbclean >" &&
 wait
 
-/usr/bin/timeout -k 45m 46m node scraper/talabat.js 0 90 dxb
-wait
-
-/usr/bin/timeout -k 45m 46m node scraper/deliveroo.js dxb
-wait
-
-/usr/bin/timeout -k 45m 46m node scraper/eateasy.js dxb
-wait
-
-/usr/bin/timeout -k 45m 46m node scraper/carriage.js dxb
-wait
-
-/usr/bin/timeout -k 45m 46m node scraper/talabat.js 91 180 dxb
-wait
-
-# sudo systemctl restart tor
+# /usr/bin/timeout -k 45m 46m node scraper/talabat.js 0 90 dxb
 # wait
-
 /usr/bin/timeout -k 45m 46m node scraper/zomato.js 0 44 dxb
 wait
-
+/usr/bin/timeout -k 45m 46m node scraper/deliveroo.js dxb
+wait
+/usr/bin/timeout -k 45m 46m node scraper/eateasy.js dxb
+wait
+/usr/bin/timeout -k 45m 46m node scraper/carriage.js dxb
+wait
+# /usr/bin/timeout -k 45m 46m node scraper/talabat.js 91 180 dxb
+# wait
 /usr/bin/timeout -k 45m 46m node scraper/zomato.js 45 87 dxb
 wait
 
 ####################### SHJ!!!!!!!!!
-/usr/bin/timeout -k 45m 46m node scraper/talabat.js 0 50 shj
-wait
+# /usr/bin/timeout -k 45m 46m node scraper/talabat.js 0 50 shj
+# wait
 
 /usr/bin/timeout -k 45m 46m node scraper/zomato.js 0 34 shj
 wait
 
-/usr/bin/timeout -k 45m 46m node scraper/talabat.js 51 99 shj
-wait
+# /usr/bin/timeout -k 45m 46m node scraper/talabat.js 51 99 shj
+# wait
 
 /usr/bin/timeout -k 45m 46m node scraper/eateasy.js shj
 wait
 
-####################### SHJ!!!!!!!!!
+####################### ABUDHABI !!!!!!!!!
+echo "< zomato ..." && /usr/bin/timeout -k 15m 16m node scraper/zomato.js 0 31 ad && echo "... zomato >" &&
+wait
+echo "< deliveroo ..." && /usr/bin/timeout -k 15m 16m node scraper/deliveroo.js ad && echo "... deliveroo >" &&
+wait
+echo "< eateasy ..." && /usr/bin/timeout -k 15m 16m node scraper/eateasy.js ad && echo "... eateasy >" &&
+wait
+echo "< carriage ..." && /usr/bin/timeout -k 15m 16m node scraper/carriage.js ad && echo "... carriage >" &&
+wait
+####################### REINDEX !!!!!!!!!
 
 node scraper/reindex.js dxb
 wait
-
 node scraper/reindex.js shj
+wait
+node scraper/reindex.js ad
 wait
 
 node devops/slackBot.js "All Scrapers Done"
@@ -100,10 +101,15 @@ wait
 echo date >> /tmp/cron_timestamps.txt
 wait
 
-pkill -f -- "chromium-browser --type=renderer" &&
-pkill chrome &&
-pkill chromium &&
-pkill -f chromium &&
-pkill -f puppeteer &&
+pkill -f -- "chromium-browser --type=renderer"
+wait
+pkill chrome
+wait
+pkill chromium
+wait
+pkill -f chromium
+wait
+pkill -f puppeteer
+wait
 kill $(ps aux | grep 'puppet' | awk '{print $2}')
 wait
