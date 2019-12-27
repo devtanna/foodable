82fb1c23b4438aa4a696a1343e18144134800a63
arayess <therayess@gmail.com>
2019-12-28 00:03:58 +0400

    [azure-deploy]: a fix for redirect qs params

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import { redirectToPage } from '../helpers/utils';
 import Cookies from 'universal-cookie';
 import base64 from 'base-64';
+import qs from 'qs';
 
 const Index = () => <div />;
 
 Index.getInitialProps = async ({ req, res, query }) => {
-  const utmSource = query['utm_source'];
-
   let cookies;
 
   if (res) {
@@ -16,21 +15,19 @@ Index.getInitialProps = async ({ req, res, query }) => {
   }
 
   let selectedLocation;
+  let landingPage;
 
   try {
     selectedLocation = JSON.parse(base64.decode(cookies.get('fdb_location')));
-    let lp = `/${selectedLocation.city}/${selectedLocation.slug}/`;
-    if (utmSource) {
-      lp = `${lp}?utm_source=pwa`;
-    }
-    redirectToPage(res, lp);
-    return;
+    landingPage = `/${selectedLocation.city}/${selectedLocation.slug}/`;
   } catch (e) {
-    let lp = '/select-area';
-    if (utmSource) {
-      lp = `${lp}?utm_source=pwa`;
+    landingPage = '/select-area';
+  } finally {
+    let queryString = qs.stringify(query);
+    if (queryString) {
+      landingPage = `${landingPage}?${queryString}`;
     }
-    redirectToPage(res, lp);
+    redirectToPage(res, landingPage);
     return;
   }
 
